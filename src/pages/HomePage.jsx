eca051280c5eee2961704ffc78fa018a8c43bd35
Paphos <15867696+Paphos@users.noreply.github.com>
2022-07-07 05:52:22 +0900

    Add topics

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -20,26 +20,44 @@ export function HomePage() {
 
   return (
     <div style={{
-      textAlign: 'center',
-      marginTop: '35px'
+      marginTop: '5px'
     }}>
-      <Link to={`./review?hidden=source`} className="btn-home btn-type1">
-        <span>{langFlagEmojis.ko} <Arrow/> {langFlagEmojis[langCode]}</span>
-      </Link>
-      <Link to={`./review?hidden=target`} className="btn-home btn-type1">
-        <span>{langFlagEmojis[langCode]} <Arrow/> {langFlagEmojis.ko}</span>
-      </Link>
-      <Link to={`./word-list`} className="btn-home btn-type2">
-        <span>Word list</span>
-      </Link>
+      <Line langCode={langCode} topicName={<>~~~~ ALL ~~~~</>} topicId="all" />
+      <Line langCode={langCode} topicName={<>Objects &#128230;</>} topicId="objects" />
+      <Line langCode={langCode} topicName={<>Places &#127969;</>} topicId="places" />
+      <Line langCode={langCode} topicName={<>Jobs &#128104;&#8205;&#127859;</>} topicId="jobs" />
+      <Line langCode={langCode} topicName={<>Verbs &#129336;</>} topicId="verbs" />
+      <Line langCode={langCode} topicName={<>Others &#128172;</>} topicId="others" />
     </div>
+  );
+}
+
+function Line({langCode, topicName, topicId}){
+  return (
+    <div  style={{
+      textAlign: 'center',
+    }}>
+      <div className="topic-title">{topicName}</div>
+      <div>
+        <Link to={`./review?hidden=source`} className="btn-home btn-type1">
+          <span>{langFlagEmojis.ko}<Arrow/>{langFlagEmojis[langCode]}</span>
+        </Link>
+        <Link to={`./review?hidden=target`} className="btn-home btn-type1">
+          <span>{langFlagEmojis[langCode]}<Arrow/>{langFlagEmojis.ko}</span>
+        </Link>
+        <Link to={`./word-list`} className="btn-home btn-type2">
+          <span>Word list</span>
+        </Link>
+      </div>
+    </div>
+    
   );
 }
 
 function Arrow() {
   return (
     <ArrowRightIcon style={{ 
-      margin: '0px 10px',
+      margin: '0px 2px',
       transform: 'translateY(5px)'
      }}/>
   )
